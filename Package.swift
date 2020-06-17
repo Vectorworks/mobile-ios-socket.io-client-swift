a0f040637694b841d93882745fcf768e3c537e6e
Ivaylo Iliev <iiliev@nemetschek.bg>
2020-06-17 11:47:15 +0300

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
-    name: "SocketIO"
+    name: "SocketIO",
+    products: [
+        .library(
+            name: "SocketIO",
+            targets: ["SocketIO-iOS"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SocketIO-iOS",
+            dependencies: [],
+            path: "./Source")
+    ]
 )
